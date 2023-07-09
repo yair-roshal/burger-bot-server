db5215eb5b9fb6389334710563d5725ecdb49bc6
yair-roshal <yair7705778@gmail.com>
2023-07-09 21:56:32 +0300

    fix

--- bot/bot.js
+++ bot/bot.js
@@ -86,21 +86,15 @@ bot.on("message", async (msg) => {
 
       let textMessageHtml = `<b>_______________________________</b>
       <b>Hello my Friend</b> 
-      
-      This is a telegram bot for sending your card with translation, pronunciation, examples and link to all meanings
-      
-      At the moment giveMeWord bot can :
-      
-      -   send messages with the required timer,
+ 
       -   the words from the dictionary are output absolutely randomly.
        
-      Link to this Bot : <a href="${link_to_bot}">Give Me Word Bot</a> 
-      
+       
       Our Group in Telegram : <a href="${link_to_public}">Very Simple English</a>
       
-      💬 If you have any questions or suggestions, email admin @yair770.
+      💬 I 
       
-      🔥 To start getting words, click on start below 👇🏻 or here /start
+      🔥 To  👇🏻 o 
       
        `
 

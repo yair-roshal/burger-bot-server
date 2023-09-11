4d9683aa6629f949493c53eaa2f7d0ca6913d58d
yair-roshal <yair7705778@gmail.com>
2023-09-11 18:15:05 +0300

    delete 26 files

--- index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-//  require('dotenv').config({ path: './.env' })
-
-var { bot } = require('./bot/bot')
-var webServer = require('./server/server')
-
-webServer(bot)
--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'),
-    router = express.Router(),
-    ordersRoutes = require('./orders.routes')
-
-router.use('/', ordersRoutes)
- 
- 
-module.exports = router

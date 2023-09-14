f78d80157cc60eec66efc4c0f9cb2e39b52436c3
yair-roshal <yair7705778@gmail.com>
2023-09-14 13:07:10 +0300

    update orders.service.js

--- server/services/orders.service.js
+++ server/services/orders.service.js
@@ -29,20 +29,16 @@ class OrdersService {
   }
 
   async createOrder(req, res) {
-    const orderData = req.body // Assuming req.body contains the order data
+    await this.connectToDatabase()
+
+    const orderData = req.body
 
     try {
       const sqlQuery = `INSERT INTO orders 
                         (queryId, cartItems, comment, totalPrice, address, optionDelivery, user_id, user_name, order_date, paymentMethod) 
                         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
 
-      const connection = await mysql.createConnection({
-        host: "194.31.175.248", // Replace with your IP address
-        port: 3306, // Replace with your port
-        user: "gen_user", // Replace with your username
-        password: "w07613vrm1", // Replace with your password
-        database: "burger_db", // Replace with your database name
-      })
+      // const connection = await mysql.createConnection(sqlConfig)
 
       const values = [
         orderData.queryId,
@@ -57,7 +53,9 @@ class OrdersService {
         orderData.paymentMethod,
       ]
 
-      const [results] = await connection.execute(sqlQuery, values)
+      const [results] = await this.connection.execute(sqlQuery, values)
+
+      // const [results] = await connection.execute(sqlQuery, values)
 
       console.log("New order saved to DB:", results)
       console.log("values111", values)

92ad1edbcac15ff7dadef0cf5e52500594baa1f1
yair-roshal <yair7705778@gmail.com>
2023-09-19 09:21:43 +0300

    update orders.controller.js

--- server/controllers/orders.controller.js
+++ server/controllers/orders.controller.js
@@ -14,46 +14,48 @@ class OrdersController {
   //     if (result) return res.status(200).send(result)
   //     else return res.status(500).send({ message: 'error.' })
   // }
-
+//=============================================================
   async getOrders(req, res) {
     const result = await OrdersService.getOrders(req, res)
 
     if (result) return res.status(200).send(result)
     else return res.status(500).send({ message: "error_getOrders" })
   }
-
+//=============================================================
   // async createOrder(req, res) {
   //   const result = await OrdersService.createOrder(req, res)
 
   //   if (result) return res.status(200).send(result)
   //   else return res.status(500).send({ message: "server_error_createOrder" })
   // }
+  
+  //=============================================================
 
-  async pay_credit_card(req, res) {
-    console.log("pay_credit_card !!!--->>>")
-    try {
-      const result = await OrdersService.pay_credit_card(req, res)
+  // async pay_credit_card(req, res) {
+  //   console.log("pay_credit_card !!!--->>>")
+  //   try {
+  //     const result = await OrdersService.pay_credit_card(req, res)
 
-      if (result) {
-        console.log("success-200  !!!--->>>")
-        // return res.status(200).send(result)
-        return res
-          .status(200)
-          .json({ titleStatus: "success-200", result: result })
-      }
-    } catch (error) {
-      console.log("error.message__pay_credit_card !!!--->>>", error.message)
+  //     if (result) {
+  //       console.log("success-200  !!!--->>>")
+  //       // return res.status(200).send(result)
+  //       return res
+  //         .status(200)
+  //         .json({ titleStatus: "success-200", result: result })
+  //     }
+  //   } catch (error) {
+  //     console.log("error.message__pay_credit_card !!!--->>>", error.message)
 
-      // return res.status(500).send({ message: "server_error_ pay_credit_card" })
+  //     // return res.status(500).send({ message: "server_error_ pay_credit_card" })
 
-      return res
-        .status(500)
-        .json({
-          titleStatus: "error on server - 500 - pay_credit_card",
-          details: error.message,
-        })
-    }
-  }
+  //     return res
+  //       .status(500)
+  //       .json({
+  //         titleStatus: "error on server - 500 - pay_credit_card",
+  //         details: error.message,
+  //       })
+  //   }
+  // }
 
   // async pay_credit_card(req, res) {
   //   const result = await OrdersService.pay_credit_card(req, res)
@@ -63,6 +65,26 @@ class OrdersController {
   //     return res.status(500).send({ message: "server_error_ pay_credit_card" })
   // }
 
+  
+  async pay_credit_card(req, res) {
+    try {
+      const result = await OrdersService.pay_credit_card(req, res);
+  
+      if (result) {
+        return res.status(200).send(result);
+      } else {
+        return res.status(500).send({ message: "server_error_pay_credit_card" });
+      }
+    } catch (error) {
+      console.error("Error in pay_credit_card:", error);
+      return res.status(500).send({ message: "An error occurred during payment" });
+    }
+  }
+
+  
+  //=============================================================
+  
+  
   async create_order_db(req, res) {
     const result = await OrdersService.create_order_db(req, res)
 

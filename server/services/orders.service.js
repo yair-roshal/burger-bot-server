5d865b0c312cff3214ae22c84d103ae5409a4c20
yair-roshal <yair7705778@gmail.com>
2023-09-20 08:43:32 +0300

    delete 1 file and update 1 file

--- server/services/orders.service.js
+++ server/services/orders.service.js
@@ -5,39 +5,35 @@ const https = require("https")
 
 class OrdersService {
   constructor() {
-    this.connection = null
+    this.pool = mysql.createPool(sqlConfig) // Создаем пул соединений
   }
 
-  async connectToDatabase() {
-    if (!this.connection) {
-      this.connection = await mysql.createConnection(sqlConfig)
+  // Метод для выполнения запросов к базе данных
+  async executeQuery(sqlQuery, values) {
+    const connection = await this.pool.getConnection()
+
+    try {
+      const [results] = await connection.execute(sqlQuery, values)
+      return results
+    } catch (error) {
+      console.error("Error executing SQL query:", error)
+      throw error
+    } finally {
+      connection.release() // Вернуть соединение в пул после использования
     }
   }
 
   // getOrders ================================================
   async getOrders() {
-    try {
-      await this.connectToDatabase()
-
-      const sqlQuery = "SELECT * FROM orders"
-      const [results] = await this.connection.execute(sqlQuery)
-
-      console.log("Orders from DB:", !!results )
-      console.log("results.length:", results.length)
-
-      return results
-    } catch (error) {
-      console.error("Error executing SQL query:", error)
-      throw error
-    }
+    const sqlQuery = "SELECT * FROM orders"
+    return this.executeQuery(sqlQuery, [])
   }
 
   async createOrder(req, res) {
     const orderData = req.body
-
     const values = [
       orderData.queryId,
-      JSON.stringify(orderData.cartItems), // Convert cartItems to a JSON string
+      JSON.stringify(orderData.cartItems),
       orderData.comment,
       orderData.totalPrice,
       orderData.address,
@@ -48,98 +44,30 @@ class OrdersService {
       orderData.paymentMethod,
     ]
 
-    axios
-      .post("https://burgerim.ru:3000/pay_credit_card", {
+    // Ваш код для отправки запроса к другой службе
+    try {
+      const response = await axios.post("https://burgerim.ru:3000/pay_credit_card", {
         firstName: "Fred",
         lastName: "Flintstone",
       })
-      .then((res) => {
-        console.log(`statusCode: ${res.statusCode}`)
-        console.log(res)
-      })
-      .catch((error) => {
-        console.error(error)
-      })
+      console.log(`statusCode: ${response.status}`)
+      console.log(response.data)
+    } catch (error) {
+      console.error("Error sending request to another service:", error)
+      throw error
+    }
   }
 
   // pay_credit_card ================================================
-
   async pay_credit_card(req, res) {
-    const queryParameters = {
-      supplier: "burger", // 'terminal_name' should be replaced by actual terminal name
-      sum: "45.70",
-      currency: "1", // ILS
-      ccno: "12312312", // Test card number
-      expdate: "0824", // Card expiry date: mmyy
-    }
-
-    // Prepare query string
-    const queryString = Object.entries(queryParameters)
-      .map(([name, value]) => `${name}=${value}`)
-      .join("&")
-
-    const tranzilaApiHost = "secure5.tranzila.com"
-    const tranzilaApiPath = "/cgi-bin/tranzila71u.cgi"
-    const url = `https://${tranzilaApiHost}${tranzilaApiPath}`
-
-    return axios
-      .post(url, queryString, {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        httpsAgent: new https.Agent({ rejectUnauthorized: false }), // Disable SSL verification (not recommended for production)
-      })
-      .then((response) => {
-        const result = response.data
-
-        // Preparing associative array with response data
-        const responseArray = result.split("&")
-        const responseAssoc = {}
-
-        for (const value of responseArray) {
-          const [key, val] = value.split("=")
-          responseAssoc[key] = val
-        }
-
-        // Analyze the result string
-        if (!responseAssoc["Response"]) {
-          console.log("result_1111", result)
-          /**
-           * When there is no 'Response' parameter it either means
-           * that some pre-transaction error happened (like authentication
-           * problems), in which case the result string will be in HTML format,
-           * explaining the error, or the request was made for generate token only
-           * (in this case the response string will contain only 'TranzilaTK'
-           * parameter)
-           */
-        } else if (responseAssoc["Response"] !== "000") {
-          console.log(responseAssoc["Response"])
-          // Any other than '000' code means transaction failure
-          // (bad card, expiry, etc..)
-        } else {
-          console.log("Success")
-          console.log(
-            'responseAssoc["Response"]--->',
-            responseAssoc["Response"]
-          )
-          console.log("result2222", result)
-          return result
-        }
-      })
-      .catch((error) => {
-        console.error(error)
-      })
+    // Ваш код для отправки запроса к другой службе
   }
 
   async create_order_db(req, res) {
-    // saving New order to DB ================================================
-    //create_order_db
-
     const orderData = req.body
-
     const values = [
       orderData.queryId,
-      JSON.stringify(orderData.cartItems), // Convert cartItems to a JSON string
+      JSON.stringify(orderData.cartItems),
       orderData.comment,
       orderData.totalPrice,
       orderData.address,
@@ -150,23 +78,11 @@ class OrdersService {
       orderData.paymentMethod,
     ]
 
-    await this.connectToDatabase()
+    const sqlQuery = `INSERT INTO orders 
+                      (queryId, cartItems, comment, totalPrice, address, optionDelivery, user_id, user_name, order_date, paymentMethod) 
+                      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
 
-    try {
-      const sqlQuery = `INSERT INTO orders 
-                        (queryId, cartItems, comment, totalPrice, address, optionDelivery, user_id, user_name, order_date, paymentMethod) 
-                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
-
-      const [results] = await this.connection.execute(sqlQuery, values)
-
-      console.log("New order saved to DB:", results)
-      console.log("values_create_order_db", values)
-
-      return results
-    } catch (error) {
-      console.error("Error executing SQL query:", error)
-      throw error // Re-throw the error to handle it elsewhere if needed
-    }
+    return this.executeQuery(sqlQuery, values)
   }
 }
 

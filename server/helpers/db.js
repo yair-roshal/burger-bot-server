bb382f42f96eed1f79d8a1082b64ff43481d2bd0
yair-roshal <yair7705778@gmail.com>
2023-10-24 15:07:58 +0300

    update config.js and db.js

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -1,34 +1,34 @@
-const config = require('config.json');
-const mysql = require('mysql2/promise');
-const { Sequelize } = require('sequelize');
+const config = require("config.json")
+const mysql = require("mysql2/promise")
+const { Sequelize } = require("sequelize")
 const { sqlConfig } = require("../../constants/config")
 
-module.exports = db = {};
+module.exports = db = {}
 
-initialize();
+initialize()
 
 async function initialize() {
-    // create db if it doesn't already exist
-    const { host, port, user, password, database } = config.database;
-    
-    // console.log('config.database----------->>>>>>>', config.database)
-    // console.log('sqlConfig----------->>>>>>>', sqlConfig) 
-    // pool = mysql.createPool(sqlConfig) // Создаем пул соединений 
-    // const connection = await mysql.createConnection(sqlConfig);
-    console.log('{ host, port, user, password }', { host, port, user, password })
-    // const connection = await mysql.createConnection({ host, port, user, password });
-    const connection = await mysql.createConnection({ host, port, user, password, database });
+  // create db if it doesn't already exist
+  // const { host, port, user, password, database } = config.database;
+  const { host, port, user, password, database } = sqlConfig
 
-    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+  console.log("sqlConfig----------->>>>>>>", sqlConfig)
+  console.log("database----------->>>>>>>", sqlConfig.database)
 
-    // connect to db
-    const sequelize = new Sequelize(database, user, password, {   host ,
-    dialect: 'mysql' });
+  const connection = await mysql.createConnection(sqlConfig)
+  //  const connection = await mysql.createConnection({ host, port, user, password, database });
 
-    // init models and add them to the exported db object
-    db.User = require('../models/user.model')(sequelize);
-    // db.User = require('../users/user.model')(sequelize);
+  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`)
 
-    // sync all models with database
-    await sequelize.sync();
+  // connect to db
+  const sequelize = new Sequelize(database, user, password, {
+    host,
+    dialect: "mysql",
+  })
+
+  // init models and add them to the exported db object
+  db.User = require("../models/user.model")(sequelize)
+
+  // sync all models with database
+  await sequelize.sync()
 }

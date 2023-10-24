cec5523cb62e47f819ceacc20486089e961b738b
yair-roshal <yair7705778@gmail.com>
2023-10-24 15:51:06 +0300

    update server.js

--- server/server.js
+++ server/server.js
@@ -80,35 +80,42 @@ module.exports = (bot) => {
       paymentMethod,
     } = req.body
 
-    for (let index = 0; index < cartItems.length; index++) {
-      const item = cartItems[index]
-      // for (const item of cartItems) {
+    console.log('for_proshli_num_paamim')
+    
+    for (const item of cartItems) {
+      console.log('item_send_sms_tele', item)
+      const itemPrice = (item.price * item.quantity).toFixed(2) || ""
 
-      if (item.quantity > 0) {
-        // Проверяем, есть ли товар в заказе
-
-        console.log("item_send_sms_tele", item)
-        const itemPrice = (item.price * item.quantity).toFixed(2) || ""
-
-        productsQuantityPrice +=
-          `<b>${item.title} = </b>${item.price}₪ * ${item.quantity} = ${itemPrice}₪` +
-          `\n`
-        console.log("productsQuantityPrice_1--->>", productsQuantityPrice)
-
-        if (item.toppings?.length > 0) {
-          for (const topping of item.toppings) {
-            if (topping.count > 0) {
-              const toppingPrice =
-                (topping.price * item.quantity).toFixed(2) || ""
-              productsQuantityPrice +=
-                ` - ${topping.title} = ${topping.price}₪ * ${item.quantity} = ${toppingPrice}₪` +
-                "\n"
-            }
-          }
-        }
-        console.log("productsQuantityPrice_2--->>", productsQuantityPrice)
-      }
+      productsQuantityPrice =
+        productsQuantityPrice +
+        `<b>${item.title}</b> * ${item.quantity} = ${itemPrice} ₪` +
+        `\n`
     }
+    
+    console.log('productsQuantityPrice', productsQuantityPrice)
+    
+    // for (const item of cartItems) {
+    //   console.log("item_send_sms_tele", item)
+    //   const itemPrice = (item.price * item.quantity).toFixed(2) || ""
+
+    //   productsQuantityPrice +=
+    //     `<b>${item.title} = </b>${item.price}₪ * ${item.quantity} = ${itemPrice}₪` +
+    //     `\n`
+    //   console.log("productsQuantityPrice_1--->>", productsQuantityPrice)
+
+    //   if (item.toppings?.length > 0) {
+    //     for (const topping of item.toppings) {
+    //       if (topping.count > 0) {
+    //         const toppingPrice =
+    //           (topping.price * item.quantity).toFixed(2) || ""
+    //         productsQuantityPrice +=
+    //           ` - ${topping.title} = ${topping.price}₪ * ${item.quantity} = ${toppingPrice}₪` +
+    //           "\n"
+    //       }
+    //     }
+    //   }
+    //   console.log("productsQuantityPrice_2--->>", productsQuantityPrice)
+    // }
 
     try {
       await bot.answerWebAppQuery(queryId, {

12fcb1a7e12a47b0be3e2de16868515b711d178d
yair-roshal <yair7705778@gmail.com>
2023-11-15 15:03:33 +0200

    update dishes.service.js

--- server/services/dishes.service.js
+++ server/services/dishes.service.js
@@ -1,11 +1,10 @@
-const mysql = require("mysql2/promise")
-const { sqlConfig } = require("../../constants/config")
-const axios = require("axios")
-const https = require("https")
-const { generateDateTime } = require("../helpers/utils")
+const mysql = require('mysql2/promise')
+const { sqlConfig } = require('../../constants/config')
+const axios = require('axios')
+const https = require('https')
+const { generateDateTime } = require('../helpers/utils')
 
 class dishesService {
-  
   constructor() {
     this.pool = mysql.createPool(sqlConfig) // Создаем пул соединений
   }
@@ -18,58 +17,59 @@ class dishesService {
       const [results] = await connection.execute(sqlQuery, values)
       return results
     } catch (error) {
-      console.error("Error executing SQL query:", error)
+      console.error('Error executing SQL query:', error)
       throw error
     } finally {
       connection.release() // Вернуть соединение в пул после использования
     }
   }
-  
-  
-    // createDish ================================================
-    async createDish({ title, price, image, description, toppings, restaurant_name }) {
-      const sqlQuery = `
+
+  // createDish ================================================
+  async createDish({ title, price, image, description, toppings, restaurant_name }) {
+    const sqlQuery = `
         INSERT INTO dishes (title, price, image, description, restaurant_name)
         VALUES (?, ?, ?, ?, ?)
       `
-  
-      try {
-        // Insert the dish into the 'dishes' table
-        const result = await this.executeQuery(sqlQuery, [title, price, image, description, restaurant_name])
-  
-        // If toppings are provided, insert them into the 'toppings' table
-        if (toppings && toppings.length > 0) {
-          const dishId = result.insertId
-          await this.insertToppings(dishId, toppings)
-        }
-  
-        return result
-      } catch (error) {
-        console.error("Error creating dish:", error)
-        throw error
+
+    try {
+      // Check for undefined values and replace them with null
+      const values = [title, price, image || null, description || null, restaurant_name]
+
+      // Insert the dish into the 'dishes' table
+      const result = await this.executeQuery(sqlQuery, values)
+
+      // If toppings are provided, insert them into the 'toppings' table
+      if (toppings && toppings.length > 0) {
+        const dishId = result.insertId
+        await this.insertToppings(dishId, toppings)
       }
+
+      return result
+    } catch (error) {
+      console.error('Error creating dish:', error)
+      throw error
     }
-  
-    // Helper method to insert toppings for a dish
-    async insertToppings(dishId, toppings) {
-      const values = toppings.map(topping => [dishId, topping.title, topping.price, topping.image])
-      const sqlQuery = `
+  }
+
+  // Helper method to insert toppings for a dish
+  async insertToppings(dishId, toppings) {
+    const values = toppings.map((topping) => [dishId, topping.title, topping.price, topping.image])
+    const sqlQuery = `
         INSERT INTO toppings (dish_id, title, price, image)
         VALUES ?
       `
-  
-      try {
-        // Insert toppings into the 'toppings' table
-        await this.executeQuery(sqlQuery, [values])
-      } catch (error) {
-        console.error("Error inserting toppings:", error)
-        throw error
-      }
+
+    try {
+      // Insert toppings into the 'toppings' table
+      await this.executeQuery(sqlQuery, [values])
+    } catch (error) {
+      console.error('Error inserting toppings:', error)
+      throw error
     }
+  }
 
   // getDishes ================================================
   async getDishes() {
-    
     const sqlQuery = `SELECT
      m.id AS id, 
      m.title,
@@ -95,7 +95,7 @@ class dishesService {
 
   // getDishesByRestaurantName ================================================
   async getDishesByRestaurantName(restaurant_name) {
-    console.log("restaurant_name", restaurant_name)
+    console.log('restaurant_name', restaurant_name)
     const sqlQuery = `
     SELECT
       m.id AS id, 
@@ -121,18 +121,15 @@ class dishesService {
 
     return this.executeQuery(sqlQuery, [restaurant_name])
   }
-  
-  
-  
 
   // getToppings ================================================
   async getToppings() {
-    const sqlQuery = "SELECT * FROM toppings"
+    const sqlQuery = 'SELECT * FROM toppings'
     return this.executeQuery(sqlQuery, [])
   }
   // getCategories ================================================
   async getCategories() {
-    const sqlQuery = "SELECT * FROM categories"
+    const sqlQuery = 'SELECT * FROM categories'
     return this.executeQuery(sqlQuery, [])
   }
 }

33f6b0a765e353f871a7bbbb6e9971d4358e2153
yair-roshal <yair7705778@gmail.com>
2023-11-17 11:29:59 +0200

    delete 1 file and update 1 file

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,10 @@ const express = require("express")
 const router = express.Router()
 const ordersRoutes = require("./orders.routes")
 const dishesRoutes = require("./dishes.routes")
-// const imagesRoutes = require("./images.routes")
+const settingsRoutes = require("./settings.routes") // Изменил название переменной на settingsRoutes
 
 router.use("/", ordersRoutes)
 router.use("/", dishesRoutes)
-// router.use("/images/", imagesRoutes)
+router.use("/", settingsRoutes) // Изменил настройки на settingsRoutes
 
 module.exports = router
---
+++ server/routes/settings.routes.js
@@ -0,0 +1,8 @@
+// settings.routes.js
+const express = require("express")
+const router = express.Router()
+const SettingsController = require("../controllers/settings.controller.js")
+
+router.route("/settings/:restaurant_id").put(SettingsController.updateSettings)
+
+module.exports = router

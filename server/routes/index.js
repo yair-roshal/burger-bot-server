9aba6813bf02d26f886e322eb99d6e03bb96b5ba
yair-roshal <yair7705778@gmail.com>
2023-12-02 18:23:01 +0200

    update burgerim.ru.crt, server/routes/index.js, toppings.routes.js and server.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,11 +5,13 @@ const dishesRoutes = require("./dishes.routes");
 const settingsRoutes = require("./settings.routes");
 const toppingsRoutes = require("./toppings.routes");
 const extrasRoutes = require("./extras.routes");
+const typesRoutes = require("./types.routes");
 
 router.use("/", ordersRoutes);
 router.use("/", dishesRoutes);
 router.use("/", settingsRoutes);
 router.use("/", toppingsRoutes);
 router.use("/", extrasRoutes);
+router.use("/", typesRoutes);
 
 module.exports = router;
--- server/routes/toppings.routes.js
+++ server/routes/toppings.routes.js
@@ -3,9 +3,8 @@ const express = require("express");
 const router = express.Router();
 const ToppingsController = require("../controllers/toppings.controller.js");
 
-router.route("/toppings").post(ToppingsController.createTopping);
-
 router.route("/toppings/:restaurant_id").get(ToppingsController.getToppings);
+router.route("/toppings").post(ToppingsController.createTopping);
 router.route("/toppings/:topping_id").put(ToppingsController.updateTopping);
 router.route("/toppings/:topping_id").delete(ToppingsController.deleteTopping);
 

f9a863a3f00165fdd17e954d25db9960d577f0d8
yair-roshal <yair7705778@gmail.com>
2023-12-03 21:42:24 +0200

    update isPhotoUrl.js

--- server/helpers/isPhotoUrl.js
+++ server/helpers/isPhotoUrl.js
@@ -1,15 +1,16 @@
 function isPhotoUrl(variable) {
-	// Проверяем, является ли переменная строкой
-	if (typeof variable === "string") {
-		// Проверяем, содержит ли строка расширение файла изображения
-		return (
-			/\.(jpg|jpeg|png|gif)$/i.test(variable) ||
-			/^data:image\/(jpeg|png|gif|jpg);base64,/.test(variable)
-		);
-	}
-	// Если переменная не является строкой, то не является ссылкой на фото
-	return false;
+  // Проверяем, является ли переменная строкой
+  if (typeof variable === "string") {
+    // Проверяем, содержит ли строка расширение файла изображения
+    return (
+      /\.(jpg|jpeg|png|gif|webp|bmp|tiff|ico|svg)$/i.test(variable) || // расширения файлов изображений
+      /^data:image\/(jpg|jpeg|png|gif|webp|bmp|tiff|ico|svg);base64,/.test(
+        variable
+      ) // base64-данные изображения
+    );
+  }
+  // Если переменная не является строкой, то не является ссылкой на фото
+  return false;
 }
 
 module.exports = { isPhotoUrl };
-// export default isPhotoUrl

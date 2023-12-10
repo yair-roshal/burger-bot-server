9b81e092a6c5fc55e70101879faf180ae903cd87
yair-roshal <yair7705778@gmail.com>
2023-12-10 04:35:11 +0200

    update dishes.routes.js, server/routes/index.js and server.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ const settingsRoutes = require("./settings.routes");
 const toppingsRoutes = require("./toppings.routes");
 const extrasRoutes = require("./extras.routes");
 const typesRoutes = require("./types.routes");
+const restaurantsRoutes = require("./restaurants.routes");
 
 router.use("/", ordersRoutes);
 router.use("/", dishesRoutes);
@@ -13,5 +14,6 @@ router.use("/", settingsRoutes);
 router.use("/", toppingsRoutes);
 router.use("/", extrasRoutes);
 router.use("/", typesRoutes);
+router.use("/", restaurantsRoutes);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,24 +1,24 @@
-require("rootpath")();
-const express = require("express");
-var https = require("https");
-var fs = require("fs");
-const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const routes = require("./routes/index");
+require("rootpath")()
+const express = require("express")
+var https = require("https")
+var fs = require("fs")
+const app = express()
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const routes = require("./routes/index")
 // const errorHandler = require("middleware/error-handler")
 
-const { generateDateId } = require("./helpers/utils");
-console.log("generateDateId", generateDateId());
-const { httpsOptions, corsOptions } = require("../constants/config");
+const { generateDateId } = require("./helpers/utils")
+console.log("generateDateId", generateDateId())
+const { httpsOptions, corsOptions } = require("../constants/config")
 
 module.exports = (bot) => {
   // app.use(bodyParser.urlencoded({ extended: false }))
   // app.use(bodyParser.json())
 
   // Adjust the limits for incoming requests
-  app.use(bodyParser.json({ limit: "10mb" })); // Change the limit according to your needs
-  app.use(bodyParser.urlencoded({ limit: "10mb", extended: true })); // Change the limit according to your needs
+  app.use(bodyParser.json({ limit: "10mb" })) // Change the limit according to your needs
+  app.use(bodyParser.urlencoded({ limit: "10mb", extended: true })) // Change the limit according to your needs
 
   //=========================================================================
 
@@ -28,10 +28,10 @@ module.exports = (bot) => {
   //   "https://api.telegram.org",
   // ]
 
-  app.use(cors(corsOptions));
+  app.use(cors(corsOptions))
   // app.use(cors())
 
-  app.use("/", routes);
+  app.use("/", routes)
 
   // api routes
   // app.use("/users", require("./controllers/users.controller"))
@@ -40,10 +40,10 @@ module.exports = (bot) => {
   // app.use(errorHandler)
   // =========================================================================
 
-  let generateIdTemp = generateDateId();
+  let generateIdTemp = generateDateId()
 
   app.post("/send_sms_tele", async (req, res) => {
-    console.log("/send_sms_tele--req.body :>> ", req.body);
+    console.log("/send_sms_tele--req.body :>> ", req.body)
     const {
       queryId,
       cartItems,
@@ -52,50 +52,59 @@ module.exports = (bot) => {
       // address,
       optionDelivery,
       paymentMethod,
-    } = req.body;
+      user_name,
+    } = req.body
 
-    console.log("for_proshli_num_paamim--------------->>>");
+    console.log("for_proshli_num_paamim--------------->>>")
 
     // fix without toppings ========================================
     // for (const item of cartItems) {
     //   console.log('item_send_sms_tele', item)
     //   const itemPrice = (item.price * item.quantity).toFixed(2) || ""
 
-    //   productsQuantityPrice =
-    //     productsQuantityPrice +
+    //   productsList =
+    //     productsList +
     //     `<b>${item.title}</b> * ${item.quantity} = ${itemPrice} ₪` +
     //     `\n`
     // }
-    // console.log('productsQuantityPrice', productsQuantityPrice)
+    // console.log('productsList', productsList)
 
     // with toppings ========================================
 
-    let productsQuantityPrice = ``;
-    console.log("productsQuantityPrice", productsQuantityPrice);
+    let productsList = ``
+    console.log("productsList", productsList)
 
-    console.log("cartItems", cartItems);
+    console.log("cartItems", cartItems)
 
     for (const item of cartItems) {
-      console.log("item_send_sms_tele", item);
-      const itemPrice = (item.price * item.quantity).toFixed(2) || "";
+      console.log("item_send_sms_tele", item)
+      const itemPrice = (item.price * item.quantity).toFixed(2) || ""
 
-      productsQuantityPrice +=
+      productsList +=
         `<b>${item.title} = </b>${item.price}₪ * ${item.quantity} = ${itemPrice}₪` +
-        `\n`;
-      console.log("productsQuantityPrice_1--->>", productsQuantityPrice);
+        `\n`
+      console.log("productsList_1--->>", productsList)
 
       if (item.toppings?.length > 0) {
         for (const topping of item.toppings) {
           if (topping.count > 0) {
             const toppingPrice =
-              (topping.price * item.quantity).toFixed(2) || "";
-            productsQuantityPrice +=
+              (topping.price * item.quantity).toFixed(2) || ""
+            productsList +=
               ` - ${topping.title} = ${topping.price}₪ * ${item.quantity} = ${toppingPrice}₪` +
-              "\n";
+              "\n"
           }
         }
       }
-      console.log("productsQuantityPrice_2--->>", productsQuantityPrice);
+
+      if (item.selectedExtrasNames?.length > 0) {
+        for (const extra in item.selectedExtrasNames) {
+          productsList +=
+            ` ${extra} - ${item.selectedExtrasNames[extra]} ` + "\n"
+        }
+      }
+
+      console.log("productsList_2--->>", productsList)
     }
 
     try {
@@ -106,9 +115,9 @@ module.exports = (bot) => {
         input_message_content: {
           parse_mode: "HTML",
           message_text: `
-<b>You ordered: </b>
-  
-${productsQuantityPrice}
+<b>${user_name} thank for your order: </b>
+
+${productsList}
 ________________
 <b>Total price: </b> ${totalPrice}₪
 ________________
@@ -119,34 +128,32 @@ ________________
 ______________________________________________
 `,
         },
-      });
+      })
 
-      console.log("send_sms_tele__success-200  !!!--->>>");
-      return res
-        .status(200)
-        .json({ titleStatus: "send_sms_tele__success-200" });
+      console.log("send_sms_tele__success-200  !!!--->>>")
+      return res.status(200).json({ titleStatus: "send_sms_tele__success-200" })
     } catch (error) {
-      console.log("error.message !!!--->>>", error.message);
+      console.log("error.message !!!--->>>", error.message)
 
       return res.status(500).json({
         titleStatus: "error on server - 500 _answerWebAppQuery",
         details: error.message,
-      });
+      })
     }
-  });
+  })
 
   // const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
   // let port = 8081
 
-  let port1 = 5005;
+  let port1 = 5005
 
   app.listen(port1, () => {
-    console.log(`Server is running on port ${port1}`);
-  });
+    console.log(`Server is running on port ${port1}`)
+  })
 
-  let port = 443;
+  let port = 443
 
   https.createServer(httpsOptions, app).listen(port, () => {
-    console.log("https Web server started at port : ", port);
-  });
-};
+    console.log("https Web server started at port : ", port)
+  })
+}

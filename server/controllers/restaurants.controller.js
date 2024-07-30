61df839cd2e6f78e41f69de66a244315d269d7aa
Yair Roshal <yair7705778@gmail.com>
2024-07-30 18:47:21 +0300

    update restaurants.controller.js and restaurants.routes.js

--- server/controllers/restaurants.controller.js
+++ server/controllers/restaurants.controller.js
@@ -1,35 +1,41 @@
-const RestaurantsService = require("../services/restaurants.service.js");
+const RestaurantsService = require("../services/restaurants.service.js")
 
 class RestaurantsController {
-	async getRestaurants(req, res) {
-		const result = await RestaurantsService.getRestaurants(req, res);
+  async getRestaurants(req, res) {
+    const result = await RestaurantsService.getRestaurants(req, res)
 
-		if (result) return res.status(200).send(result);
-		else return res.status(500).send({ message: "error-getRestaurants" });
-	}
-	async getRestaurant(req, res) {
-		const result = await RestaurantsService.getRestaurant(req, res);
+    if (result) return res.status(200).send(result)
+    else return res.status(500).send({ message: "error-getRestaurants" })
+  }
 
-		if (result) return res.status(200).send(result);
-		else return res.status(500).send({ message: "error-getRestaurants" });
-	}
-	async getUserRestaurant(req, res) {
-		const result = await RestaurantsService.getUserRestaurant(req, res);
-		if (result) return res.status(200).send(result);
-		else return res.status(500).send({ message: "error-getUserRestaurant" });
-	}
-	async createRestaurant(req, res) {
-		const { userSub } = req.body;
-		
-		try {
-			const result = await RestaurantsService.createRestaurant(userSub);
-			console.log(result);
-			return res.status(200).send(result);
-		} catch (error) {
-			console.error("Error creating restaurant:", error);
-			return res.status(500).send({ message: "error-createRestaurant" });
-		}
-	}
+  async getRestaurant(req, res) {
+    const result = await RestaurantsService.getRestaurant(req, res)
+
+    if (result) return res.status(200).send(result)
+    else return res.status(500).send({ message: "error-getRestaurants" })
+  }
+
+  async getUserRestaurant(req, res) {
+    console.log("getUserRestaurant req.params :>> ", req.params)
+    const result = await RestaurantsService.getUserRestaurant(req, res)
+
+    if (result) return res.status(200).send(result)
+    else return res.status(500).send({ message: "error-getUserRestaurant" })
+  }
+
+  async createRestaurant(req, res) {
+    const { userSub } = req.body
+
+    try {
+      const result = await RestaurantsService.createRestaurant(userSub)
+
+      console.log(result)
+      return res.status(200).send(result)
+    } catch (error) {
+      console.error("Error creating restaurant:", error)
+      return res.status(500).send({ message: "error-createRestaurant" })
+    }
+  }
 }
 
-module.exports = new RestaurantsController();
+module.exports = new RestaurantsController()
--- server/routes/restaurants.routes.js
+++ server/routes/restaurants.routes.js
@@ -2,6 +2,16 @@ const express = require("express")
 const router = express.Router()
 const RestaurantsController = require("../controllers/restaurants.controller.js")
 
+// Middleware для логирования запросов
+function logRequest(req, res, next) {
+	if (req.path.startsWith("/restaurants/user/")) {
+	  console.log(`Запрос по пути: ${req.path}`);
+	}
+	next();
+  }
+  
+  router.use(logRequest);
+  
 router.route("/restaurants/").get(RestaurantsController.getRestaurants)
 router.route("/restaurants/:restaurant_id").get(RestaurantsController.getRestaurant)
 router.route("/restaurants/user/:user_sub").get(RestaurantsController.getUserRestaurant)

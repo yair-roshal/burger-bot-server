f3f1a1bc6fa6a499f95fd3727cfc65cc744fac74
Yair Roshal <yair7705778@gmail.com>
2024-08-05 17:08:47 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
+require("dotenv").config()
 //  require('dotenv').config({ path: './.env' })
 
-var { bot } = require("./bot/bot");
-var webServer = require("./server/server");
+var { bot } = require("./bot/bot")
+var webServer = require("./server/server")
 
-console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+console.log("process.env.NODE_ENV", process.env.NODE_ENV)
 
 // const token =
 //   process.env.NODE_ENV === "prod"
@@ -12,12 +12,12 @@ console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 //     : process.env.TELEGRAM_BOT_TOKEN_dev;
 
 if (process.env.NODE_ENV === "prod") {
-  console.log("1111");
-  webServer(bot);
+  console.log(`process.env.NODE_ENV === "prod"`)
+  webServer(bot)
 } else if (process.env.NODE_ENV === "dev") {
-  console.log("2222");
+  console.log(`process.env.NODE_ENV === "dev"`)
 
-  webServer();
+  webServer()
 }
 
 // webServer(bot);

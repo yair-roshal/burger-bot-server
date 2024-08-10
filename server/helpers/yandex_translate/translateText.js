a97b4ac11aa4255a886a4b486f96101334b9a972
yair-roshal <yair7705778@gmail.com>
2024-08-11 00:50:14 +0300

    update translateText.js

--- server/helpers/yandex_translate/translateText.js
+++ server/helpers/yandex_translate/translateText.js
@@ -10,7 +10,6 @@ const sleep = (ms) => {
 
 const repeatedly_request_to_translate = async (url, body, headers, n) => {
   try {
- 
     const response = await axios.post(url, body, headers)
     const translate = response.data.translations[0].text
     return translate
@@ -35,6 +34,11 @@ async function getIAMToken() {
 
 module.exports = async function translateText(texts, target_language) {
   
+  if (!texts || !Array.isArray(texts) || texts.length === 0) {
+    console.error("Invalid texts provided. Ensure that texts is a non-empty array.")
+    return null // or throw an error depending on your use case
+  }
+  
   
   const IAM_TOKEN = await getIAMToken()
 
@@ -50,6 +54,8 @@ module.exports = async function translateText(texts, target_language) {
 
   let translate
   try {
+    console.log(" url, body, headers :>> ", url, body, headers)
+
     translate = await repeatedly_request_to_translate(
       url,
       body,

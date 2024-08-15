6a0b77187595bb28ef2e6296644eb8acffad25a0
yair-roshal <yair7705778@gmail.com>
2024-08-15 17:51:54 +0300

    update ecosystem.config.ts

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -1,24 +1,22 @@
 module.exports = {
-    apps: [
+  apps: [
     {
       name: "burger_01",
-      script: "ts-node",
-      args: "-P tsconfig.json ./server/server.ts",
-      exec_mode: "cluster", // Use cluster mode for scaling
-      instances: "max", // Automatically scale based on the number of CPUs
-      interpreter: "node", // Убедитесь, что используется Node.js
+      script: "./server/server.ts", // Указываем файл для запуска
+      interpreter: "ts-node", // Указываем интерпретатор
+      interpreter_args: "-P tsconfig.json", // Аргументы для ts-node
+      exec_mode: "cluster", // Используем кластерный режим для масштабирования
+      instances: "max", // Автоматическое масштабирование на основе количества CPU
+      max_memory_restart: "300M", // Перезапуск, если память превышает этот лимит
 
-      max_memory_restart: "300M", // Restart if memory exceeds this limit
-
-      // Logging
+      // Логирование
       out_file: "./logs/out.log",
       error_file: "./logs/error.log",
       merge_logs: true,
-
       time: true,
-      log_date_format: "HH:mm:ss", // Log date format
+      log_date_format: "HH:mm:ss", // Формат даты в логах
 
-      watch: true, // Enable file watching for changes
+      watch: true, // Включаем отслеживание изменений файлов
       ignore_watch: [
         "./node_modules",
         "./.DS_Store",
@@ -30,9 +28,9 @@ module.exports = {
         "newBot-error.log",
       ],
 
-      // Env Specific Config
+      // Конфигурация окружений
       env: {
-        NODE_ENV: "dev", // Default environment
+        NODE_ENV: "dev", // По умолчанию
       },
       env_prod: {
         NODE_ENV: "prod",
@@ -43,5 +41,3 @@ module.exports = {
     },
   ],
 }
-
- 

37a8bc0825a4e194373aeea3f1e28305d71c5063
yair-roshal <yair7705778@gmail.com>
2024-08-15 18:01:32 +0300

    update ecosystem.config.ts

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -8,7 +8,7 @@ module.exports = {
       // interpreter_args: "-P tsconfig.json", // Аргументы для ts-node
       
        script: "ts-node", // or locally "./node_modules/.bin/_ts-node" 
-       args: "server/server.ts"
+       args: "server/server.ts",
     
     
       exec_mode: "cluster", // Используем кластерный режим для масштабирования

62f10af6a49d60250ade48d1158b1b89c3f2c68c
yair-roshal <yair7705778@gmail.com>
2024-08-15 17:43:42 +0300

    update ecosystem.config.ts

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -1,44 +1,47 @@
 const config: any = {
-    apps: [
-        {
-            name: 'burger_bot_server',
-            script: './build/index.ts', // Ensure that TypeScript files are supported
-            exec_mode: 'cluster', // Use cluster mode for scaling
-            instances: 'max', // Automatically scale based on the number of CPUs
-            interpreter: 'node', // Убедитесь, что используется Node.js
+  apps: [
+    {
+      name: "burger_01",
+      script: "ts-node",
+      args: "-P tsconfig.json ./server/server.ts",
+      exec_mode: "cluster", // Use cluster mode for scaling
+      instances: "max", // Automatically scale based on the number of CPUs
+      interpreter: "node", // Убедитесь, что используется Node.js
 
-            max_memory_restart: '300M', // Restart if memory exceeds this limit
+      max_memory_restart: "300M", // Restart if memory exceeds this limit
 
-            // Logging
-            out_file: './burger_bot_server_out.log',
-            error_file: './burger_bot_server_error.log',
-            time: true,
-            log_date_format: 'HH:mm:ss', // Log date format
+      // Logging
+      out_file: "./logs/out.log",
+      error_file: "./logs/error.log",
+      merge_logs: true,
 
-            watch: true, // Enable file watching for changes
-            ignore_watch: [
-                './node_modules',
-                './.DS_Store',
-                './package.json',
-                './yarn.lock',
-                '*.log',
-                '*.txt',
-                'newBot-out.log',
-                'newBot-error.log',
-            ],
+      time: true,
+      log_date_format: "HH:mm:ss", // Log date format
 
-            // Env Specific Config
-            env: {
-                NODE_ENV: 'dev', // Default environment
-            },
-            env_prod: {
-                NODE_ENV: 'prod',
-            },
-            env_dev: {
-                NODE_ENV: 'dev',
-            },
-        },
-    ],
-};
+      watch: true, // Enable file watching for changes
+      ignore_watch: [
+        "./node_modules",
+        "./.DS_Store",
+        "./package.json",
+        "./yarn.lock",
+        "*.log",
+        "*.txt",
+        "newBot-out.log",
+        "newBot-error.log",
+      ],
 
-export = config;
+      // Env Specific Config
+      env: {
+        NODE_ENV: "dev", // Default environment
+      },
+      env_prod: {
+        NODE_ENV: "prod",
+      },
+      env_dev: {
+        NODE_ENV: "dev",
+      },
+    },
+  ],
+}
+
+export = config

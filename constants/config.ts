d697bdfd68c544b225a80c8990f7e281a52dc3d5
yair-roshal <yair7705778@gmail.com>
2024-08-18 22:30:59 +0300

    update config.ts

--- constants/config.ts
+++ constants/config.ts
@@ -39,15 +39,15 @@ const allowedOrigins = [
 ]
 
 const corsOptions: CorsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
+  // origin: function (origin, callback) {
+  //   if (allowedOrigins.includes(origin) || !origin) {
+  //     callback(null, true)
+  //   } else {
+  //     callback(new Error("Not allowed by CORS"))
+  //   }
+  // },
 
-  // origin: "*", // Разрешить запросы с любого источника
+  origin: "*", // Разрешить запросы с любого источника
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"], // Разрешить все основные HTTP-методы
   allowedHeaders: [
     "Origin",

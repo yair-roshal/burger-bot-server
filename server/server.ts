fcd979381196c52c8746dddbaa5441b5e2b544b8
yair-roshal <yair7705778@gmail.com>
2024-08-18 22:31:22 +0300

    update server.ts

--- server/server.ts
+++ server/server.ts
@@ -34,16 +34,16 @@ export default (bot?: any) => {
   app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }))
 
   // console.log('corsOptions :>> ', corsOptions);
-  // app.use(cors(corsOptions))
+  app.use(cors(corsOptions))
    
  
   
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   res.setHeader('Access-Control-Allow-Origin', '*');
+  //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  //   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  //   next();
+  // });
   
   // Handle root path explicitly
   app.get("/", (req: Request, res: Response) => {
